0d2c456c32bf6925ca9afe2fcf6043049d838208
Ethan <116187182+Damadog101@users.noreply.github.com>
2023-03-30 10:04:01 -0700

    choose characters works and title screen no display works

--- script.js
+++ script.js
@@ -4,6 +4,23 @@ let raul = document.getElementById("p2")
 let moveBy = 10
 let bigMoveBy = 100
 
+let title = document.getElementById("titleWrapper")
+let startButton = document.getElementById("start")
+
+let p1Select = document.getElementById("p1Select")
+let p2Select = document.getElementById("p2Select")
+
+const playerIcons = {
+    "Amogus" : "/images/amogus.png",
+    "Cowe" : "/images/cowe.png",
+    "Shibe" : "/images/shibe.png",
+    "Raul" : "/images/raul.png"
+  };
+
+startButton.addEventListener("click", () => {
+    title.classList.add("noDisplay")
+})
+
 window.addEventListener("load", () => {
     p1.style.position = 'absolute'
     p1.style.left = '0'
@@ -50,3 +67,22 @@ window.addEventListener("keyup", (e) => {
     }
 
 })
+
+p1Select.addEventListener("change", () => {
+    let p1SelectImg = document.getElementById("selectImgOne");
+    let p1Img = document.getElementById("characterImgOne")
+    let p1 = p1Select.value
+    console.log()
+
+    p1Img.src = playerIcons[p1]
+    p1SelectImg.src = playerIcons[p1]
+})
+p2Select.addEventListener("change", () => {
+    let p2SelectImg = document.getElementById("selectImgTwo");
+    let p2Img = document.getElementById("characterImgTwo")
+    let p2 = p2Select.value
+
+    p2Img.src = playerIcons[p2]
+    p2SelectImg.src = playerIcons[p2]
+})
+
